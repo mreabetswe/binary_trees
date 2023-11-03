acbea0c5cbdc2b86854d7bfc943fa74ca8c83087
mreabetswe <reabetswemokot123@gmail.com>
2023-11-02 17:26:10 -0700

    0x1D. C - Binary trees

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -4,17 +4,22 @@
  * binary_tree_node - function that creates a binary tree node.
  * @parent: is a pointer to the parent node of the node to create.
  * @value: is the value to put in the new node.
- * Return: A pointer to the new node, or NULL on failure.
+ * Return: If succesful a pointer to the new node, or NULL on failure.
  */
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	binary_tree_t *bt = NULL;
+	binary_tree_t *tmp;
+	
+	tmp = NULL;
 
-	bt = calloc(1, sizeof(binary_tree_t));
-	if (!bt)
-		return (NULL);
-	bt->parent = parent;
-	bt->n = value;
-	return (bt);
+	tmp = calloc(1, sizeof(binary_tree_t));
+	if (tmp != NULL)
+	{
+		memset(tmp, 1, 1, sizeof(binary_tree_t));
+
+		tmp->parent = parent;
+		tmp->n = value;
+		
+		return (tmp);
 }
